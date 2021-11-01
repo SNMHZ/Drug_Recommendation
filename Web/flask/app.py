11f651b157ca714d3d0735b1b4ec0d972ca114c8
JunGiKim <junhardstudy@knu.ac.kr>
2021-11-01 11:14:54 +0900

    backend work add

--- Web/flask/app.py
+++ Web/flask/app.py
@@ -1,37 +1,102 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-import predictor_model3 as pm
+import datetime
 
 app = Flask(__name__)
+app.msg_json_data = {}
+app.result = {}
 CORS(app)
 
-@app.route('/')
+@app.route('/AndroidChatMessage', methods=['POST'])
+def messageAccept():
+    print("success accept message")
+    app.msg_json_data[0] = request.get_json()
+    msg = request.get_json()
+
+    text_body = msg['body']
+    '''
+    텍스트 전처리나 preprocessing
+    '''
+    result = modelPrediction(text_body) # model 예측
+    current_date = str(datetime.datetime.now())
+    isClear = True
+    symptoms = None
+    drugs = None
+    predicts = None
+    probs = None
+
+    result_obj = jsonify({"type": "1", "body": result, "date" : current_date,
+                          "isClear": isClear, "predicts": predicts, "probs:": probs,
+                          "symptoms": symptoms, "drugs": drugs})
+
+    print(result_obj.get_json())
+    return result_obj
+
+@app.route('/GetResult', methods=['GET'])
+def getResult():
+    if app.result[0] is not None:
+        return app.result[0]
+    else:
+        error_msg = jsonify({"type": "-1", "body": "waiting", "date": 1,
+                              "isClear": 1, "predicts": 1, "probs:": 1,
+                              "symptoms": 1, "drugs": 1})
+        return error_msg
+
+'''
+하단은 디버깅용 기능
+'''
+
+@app.route('/hello_world')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello, World!'
 
-@app.route('/test', methods=['POST'])
-def test():
-    msg = request.get_json(force=True)
+@app.route('/GetTest1', methods=['GET'])
+def get_test_one():
+    jsonObj = app.msg[0]
+    if jsonObj['body'] == 'symptom list':
+        obj = jsonify({"type": "selection_msg", "body": "ADHD, Sore, Sick, Headache"})
+        print("증상 리스트 전송")
+        return obj
+    else :
+        obj = jsonify({"type": "result_msg", "body": jsonObj['body']+"에 대한 결과"})
+        print("모델 예측 결과 메시지 전송")
+        return obj
 
-    lemed_msg = pm.lemlem(msg['text'])
-    tmp = pm.predictConditionSum(lemed_msg)
-    m_sorted_res = sorted(list(tmp.items()), key=lambda x:x[1], reverse=True)[:1000]
+@app.route('/GetTest2', methods=['GET'])
+def get_test_two():
+    return jsonify({"msg" : "hellow android - from flask"})
 
-    r_msg=''
-    for res in m_sorted_res[:1]:
-        r_msg+='( condition : '+res[0]+" / "+pm.findDrugName(res[0])+' ) \n '
-        
-    #컨디션만 리턴
-    # r_msg=''
-    # for i, res in enumerate(m_sorted_res):
-    #     if pm.isincheck(res[0]):
-    #         r_msg+=res[0]+' '
+@app.route('/users', methods=['GET']) #get echo api
+def get_echo_call(param):
+    print("client post 확인")
+    return jsonify({"param": param})
 
-    return {
-        "text" : r_msg
-    }
+@app.route('/echo_call', methods=['POST']) #post echo api
+def post_echo_call():
+    param = request.get_json()
+    return jsonify(param)
 
-
-if __name__ == '__main__':
-    app.debug = True
+if __name__ == "__main__":
     app.run()
+
+
+'''
+import asyncio
+import json
+
+import websockets
+
+async def accept(websocket, path):
+    while True:
+        print("server is running")
+        data = await websocket.recv()
+        json_object = json.loads(data)
+        print("receive : " + json_object["body"])
+        #await websocket.send(data)
+
+start_server = websockets.serve(accept, "0.0.0.0", 9889)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+'''
+
--- Web/msg_format.md
+++ Web/msg_format.md
@@ -2,8 +2,9 @@
 | key         |  type             | description |
 | -------     | -------------     | -           | 
 | seq         | `int`               | 대화 순서, -1 일 시 end message |
+| type        | `int`               | 0 : 수신, 1 : 송신 : 2 : 증상 리스트 전송 |
 | sys         | `vector<string>`    | `['os', 'model', ...]`, seq !=0 일 시 `None` |
-| msg         | `string`            | 상담 내용 |
+| body         | `string`            | 상담 내용 |
 | rating      | `int`               | 1 ~ 5, seq != -1 일 시 `None` |
 | auth        | `-`                 | 여유 있을 시 구현 |
 
