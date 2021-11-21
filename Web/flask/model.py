8c91afe38121b3f37879259f63820b0687418fe3
SNMHZ <snmhz325@gmail.com>
2021-11-21 11:24:52 +0900

    feat: Update predict model

--- Web/flask/model.py
+++ Web/flask/model.py
@@ -11,6 +11,10 @@ THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 my_model = os.path.join(THIS_FOLDER, 'mymodel/content/test-ynat/checkpoint-2000')
 model = AutoModelForSequenceClassification.from_pretrained(my_model)
 tokenizer = AutoTokenizer.from_pretrained('google/bert_uncased_L-12_H-512_A-8', use_fast=True)
+trainer = Trainer(
+        model,
+        tokenizer=tokenizer,
+    )
 
 m_dict_csv = os.path.join(THIS_FOLDER, 'dataset/top20condition.csv')
 m_dict = pd.read_csv(m_dict_csv, index_col=0)
@@ -31,10 +35,6 @@ def pred_drug(input_text):
     test_df = pd.DataFrame(test_dict, index=[0], columns=['review'])
     df_to_dataset = Dataset.from_pandas(test_df)
     encoded_datasets = df_to_dataset.map(preprocess_function, batched=True)
-    trainer = Trainer(
-        model,
-        tokenizer=tokenizer,
-    )
     output = trainer.predict(encoded_datasets)
     res = to_json(output)
     
@@ -81,4 +81,4 @@ def to_json(output):
 
 if __name__=='__main__':
     # 테스트용 입력
-    print(pred_drug("""Weight loss Cramping Diarrhea Itchy skin Joint and muscle pain Nausea and vomiting Headaches"""))
+    print(pred_drug("""My boyfriend of 8 years took this medication for migraine for a year and ahalf. It was great for his migraine but hell on his personality and character as a person. This medication made him confused, unable to make decisions, angry because he was confused. He became argumentative with me , coworkers, and family members. Every task he took on was filled with confusion and his ego. In 8 years I never seen him be so cold ..he was not the same person. I thought his behaviour was cause from some other medicine so I convince him to stop. Then the behaviour continued..after reading on this medicine I convince him to drop it. It had been 3 weeks since he took the medication and it&#039;s like night and day.  He is slowly returning to the man I know."""))
